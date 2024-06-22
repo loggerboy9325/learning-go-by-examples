2635c75d8aec941a7c76a9f3b5c03359f832dcbe
George <loggerboy9325@gmail.com>
2024-06-22 01:04:23 -0400

    finished app

--- go-rest-api/internal/main.go
+++ go-rest-api/internal/main.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/loggerboy9325/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi"
+	"github.com/loggerboy9325/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi/operations"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-	log.Println("Listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	server.Port = 8080
+
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User)
+}
+
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		URL = "https://github.com/scraly/gophers/blob/main/dr-who-gophers.png"
+	}
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("error")
+	}
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
